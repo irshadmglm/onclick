73038f8bd3c341d25fe80b4632dcf09e752c48a0
irshadmglm <127028697+irshadmglm@users.noreply.github.com>
2023-08-13 12:54:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,54 +3,77 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var http=require('http')
-var dotenv =require('dotenv').config();
+var http = require('http');
+var dotenv = require('dotenv').config();
 
-var socketIO=require('socket.io')
+var socketIO = require('socket.io');
 var userRouter = require('./routes/user');
 var adminRouter = require('./routes/admin');
-const {engine : hbs }=require("express-handlebars")
+const { engine: hbs } = require("express-handlebars")
 
-const dbUrl=process.env.DATABASE_URL;
-const apiKey=process.env.API_KEY;
-const debugMode=process.env.DEBUG === 'true';
+const dbUrl = process.env.DATABASE_URL;
+const apiKey = process.env.API_KEY;
+const debugMode = process.env.DEBUG === 'true';
 var app = express();
-var server =http.createServer(app)
-var io=socketIO(server)
-const fileUpload=require('express-fileupload')
-var db=require('./config/connection')
-var session=require('express-session')
+var server = http.createServer(app)
+var io = socketIO(server)
+const fileUpload = require('express-fileupload')
+var db = require('./config/connection')
+var session = require('express-session')
+const RedisStore = require('connect-redis')(session);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.engine('hbs',hbs({extname:'hbs',defaultLayout:'layout',layoutsDir:__dirname+'/views/layout/',partialsDir:__dirname+'/views/partials/'}))
+app.engine('hbs', hbs({
+  extname: 'hbs',
+  defaultLayout: 'layout',
+  layoutsDir: __dirname + '/views/layout/',
+  partialsDir: __dirname + '/views/partials/'
+}));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(fileUpload())
+app.use(fileUpload());
 
-app.use(session({secret:"anime",resave:false,saveUninitialized:true,cookie:{maxAge:6000000}}))
+app.use(session({
+  store: new RedisStore({
+    host: '127.0.0.1', // or 'localhost' if Redis is on the same machine
+    port: 6379,        // Default Redis port
+    // ... other Redis configuration options
+  }),
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 6000000 }
+}));
+
 app.use('/product-images', express.static(path.join(__dirname, 'product-images')));
-db.connect((err)=>{
-  if(err) console.log("connection error"+err );
- else console.log("database connected to port 27017");
-  
- })
+db.connect((err) => {
+  if (err) console.log("connection error" + err);
+  else console.log("database connected to port 27017");
+});
+
+// Attach io object to the app object
+app.io = io;
+
+// Handle public chat
 const publicChatSockets = {};
 io.on('connection', (socket) => {
   console.log('New WebSocket connection');
 
   socket.join('publicChat');
-  publicChatSockets[socket.id]=true
+  publicChatSockets[socket.id] = true;
 
   // Handle public chat message
   socket.on('publicChatMessage', (message) => {
     io.to('publicChat').emit('publicChatMessage', message);
   });
 
-  // Handle disconnection
+  // Handle public chat disconnection
   socket.on('disconnect', () => {
     console.log('public WebSocket connection disconnected');
     delete publicChatSockets[socket.id];
@@ -75,19 +98,16 @@ io.on('connection', (socket) => {
   });
 });
 
-// Attach io object to the app object
-app.io = io;
-
 app.use('/', userRouter);
 app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -96,9 +116,11 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 // Start the server
- const port =process.env.PORT || 5000;
-  server.listen(port, () => {
-     console.log(`Server is up and running on port ${port}`); });
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+  console.log(`Server is up and running on port ${port}`);
+});
 
 module.exports = app;
